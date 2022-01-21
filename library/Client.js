02e82f371fd602f5a4541c932683d5d6c5168a0c
Scorprian <43557305+ScorprianDev@users.noreply.github.com>
2022-01-22 12:24:35 +1300

    Update Client.js

--- library/Client.js
+++ library/Client.js
@@ -13,45 +13,7 @@ class Client extends EventEmitter {
 		}, options);
 	}
 
-	connect() {
-		try {
-			const gatewayURL = `wss://gateway.discord.gg/?v=${Constants.GATEWAY_VERSION}&encoding=json`;
-			const socket = new WebSocket(gatewayURL);
-			let heartbeat = null;
-
-			socket.on("message", msg => {
-				const payload = JSON.parse(msg.toString());
-				const { op, d } = payload;
-				if (heartbeat === null) heartbeat = d.heartbeat_interval;
-				const heartbeating = { op: 1, d: null };
-				const identify = {
-					"op": 2,
-					"d": {
-						"token": this.token,
-						"intents": this.options.intents,
-						"properties": {
-							"$os": "linux",
-							"$browser": Constants.LIBRARY_NAME,
-							"$device": Constants.LIBRARY_NAME
-						}
-					}
-				};
-				switch(op) {
-				case 10:
-					setInterval(() => {
-						console.log(`Sending heartbeat every ${heartbeat} miliseconds`);
-						socket.send(JSON.stringify(heartbeating));
-					}, heartbeat);
-				
-					socket.send(JSON.stringify(identify));
-					break;
-				}
-				console.log(payload);
-			});
-		} catch(e) {
-			console.log(e);
-		}
-	}
+	
 }
 
 module.exports = Client;
